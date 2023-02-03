4ad3c18072d9388229621cc62da3d3bc05115b87
julienwakim <julienjwakim@gmail.com>
2023-02-03 01:41:23 -0500

    Initialization of PropertyProbe
    Set up workspace with basic code that adds Angular to an API created in Golang. (Linked frontend to backend)

---
+++ api/platform/properties/properties.go
@@ -0,0 +1,32 @@
+package properties
+
+type Getter interface {
+	GetAll() []Property
+}
+
+type Adder interface {
+	Add(property Property)
+}
+
+type Property struct {
+	Owner   string `json:"owner"`
+	Address string `json:"address"`
+}
+
+type Repo struct {
+	Properties []Property
+}
+
+func New() *Repo {
+	return &Repo{
+		Properties: []Property{},
+	}
+}
+
+func (r *Repo) Add(property Property) {
+	r.Properties = append(r.Properties, property)
+}
+
+func (r *Repo) GetAll() []Property {
+	return r.Properties
+}
