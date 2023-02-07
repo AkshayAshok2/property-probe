09186e530a51c0c888f05889bfff0efa4d27e418
cseschmidt <113317598+cseschmidt@users.noreply.github.com>
2023-02-07 16:05:11 -0600

    Update scrape.go

--- scrape.go
+++ scrape.go
@@ -27,14 +27,33 @@ func main() {
 		var date string = strconv.Itoa(i)
 	}
 	*/
-	var date string = "06"
+	fmt.Println("HI")
+	var date string = "25"
 
-	scrapeURL := "https://alachua.realforeclose.com/index.cfm?zaction=AUCTION&Zmethod=PREVIEW&AUCTIONDATE=02/" + date + "/2023"
+	scrapeURL := "https://alachua.realforeclose.com/index.cfm?zaction=AUCTION&Zmethod=PREVIEW&AUCTIONDATE=08/" + date + "/2022"
 
-	c.OnHTML("div.Head_C div[id=Area_C] div[tabindex=0] div.AUCTION_DETAILS tr", func(e *colly.HTMLElement) {
+	c.OnHTML("div.Sub_Title", func(e *colly.HTMLElement) {
 
-		fmt.Println(e.ChildText("td.AD_DTA"))
+		fmt.Println(e.Text)
+
+		//fmt.Println(e.ChildText(e.Text))
 		//fmt.Println(e.ChildText("td.AD_DTA"))
+
+	})
+
+	c.OnHTML("div.Auct_Area", func(e *colly.HTMLElement) {
+		selection := e.DOM
+		fmt.Println(selection.Find("div.AUCTION_DETAILS > table > tbody > tr:nth-child(3) > td").Text())
+	})
+
+	c.OnXML("//tr //td", func(e *colly.XMLElement) {
+		fmt.Println("ON XML: " + e.Text)
+	})
+
+	c.OnHTML("tbody tr", func(e *colly.HTMLElement) {
+		fmt.Print("Childe: ")
+		fmt.Println(e.ChildText)
+
 	})
 
 	c.Visit(scrapeURL)
