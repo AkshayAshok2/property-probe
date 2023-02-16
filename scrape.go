e2e7eac8416d96e4c7eddc41b0c253e2d597f974
cseschmidt <113317598+cseschmidt@users.noreply.github.com>
2023-02-15 20:37:13 -0600

    Update scrape.go

--- scrape.go
+++ scrape.go
@@ -9,7 +9,6 @@ import (
 	"github.com/gocolly/colly"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	//"strconv"
 )
 
 type ForeclosureProperty struct {
@@ -23,10 +22,10 @@ type ForeclosureProperty struct {
 func connectAndLoadData(w http.ResponseWriter, r *http.Request) {
 
 	property := ForeclosureProperty{
-		address: "123",
-		price: "1000",
-		bed: "3",
-		bath: "4",
+		address:       "123",
+		price:         "1000",
+		bed:           "3",
+		bath:          "4",
 		squareFootage: "2000",
 	}
 	var dsn = "go:Gators123@tcp(cen3031-project.mysql.database.azure.com)/listings?charset=utf8&parseTime=True&Ioc=Local"
@@ -42,20 +41,11 @@ func connectAndLoadData(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
 // whatever is inside main runs
 func main() {
-	http.HandleFunc("/CreateStuff",connectAndLoadData)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	/*http.HandleFunc("/CreateStuff",connectAndLoadData)
+	log.Fatal(http.ListenAndServe(":8080", nil))*/
 	c := colly.NewCollector(colly.AllowedDomains("alachua.realforeclose.com"))
-
-	for i := 1; i < 31; i++ {
-	if i < 10{
-		var date string = "0" + strconv.Itoa(i)
-	} else {
-		var date string = strconv.Itoa(i)
-	}
-	
 
 	//Initializes the days and strings to loop through the urls and gather foreclosure data for each day
 	//var month string = "02"
@@ -75,30 +65,30 @@ func main() {
 				var day string = strconv.Itoa(i)
 			}
 		}
-	}
-	*/
+	}*/
+
+	var month string = "09"
+	var day string = "20"
 
 	scrapeURL := "https://alachua.realforeclose.com/index.cfm?zaction=AUCTION&Zmethod=PREVIEW&AUCTIONDATE=" + month + "/" + day + "/2022"
 
-	c.OnHTML("div.Sub_Title", func(e *colly.HTMLElement) {
+	//c.OnHTML("div.Auct_Area", func(e *colly.HTMLElement) {
+	//	selection := e.DOM
+	//	fmt.Println(selection.Find("div.AUCTION_DETAILS > table > tbody > tr:nth-child(3) > td").Text())
+	//})
 
-		fmt.Println(e.Text)
+	c.OnHTML("body.main_text.div.headerMenu" /*body.main_text.table.MAIN_TBL.tbody" /*.tr.td.MAIN_TBL_CONTENT"*/, func(e *colly.HTMLElement) {
+		e.ForEach("a", func(_ int, e *colly.HTMLElement) {
+			var contentHeader string = e.Text
 
-		//fmt.Println(e.ChildText(e.Text))
-		//fmt.Println(e.ChildText("td.AD_DTA"))
-
-	})
-
-	c.OnHTML("div.Auct_Area", func(e *colly.HTMLElement) {
-		selection := e.DOM
-		fmt.Println(selection.Find("div.AUCTION_DETAILS > table > tbody > tr:nth-child(3) > td").Text())
-	})
-
-	c.OnHTML("tbody tr", func(e *colly.HTMLElement) {
-		fmt.Print("Child: ")
-		fmt.Println(e.ChildText)
+			fmt.Println(contentHeader)
+		})
+		//})
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
 
 	})
+	c.Visit(scrapeURL)
 
 	var scrapeAddress string
 	var scrapeBath string
@@ -112,17 +102,16 @@ func main() {
 	property.bed = scrapeBed
 	property.price = scrapePrice
 	property.squareFootage = scrapeSquareFootage
-
-	c.Visit(scrapeURL)
-	/*
-	   	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", "go", "Gators123", host, port, database)
-	   	// Open the connection
-	   	db, err := sql.Open("mysql", dsn)
-	   	if err != nil {
-	       		log.Fatalf("impossible to create the connection: %s", err)
-	   	}
-	   	defer db.Close()
-	*/
 }
+
+/*
+   	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", "go", "Gators123", host, port, database)
+   	// Open the connection
+   	db, err := sql.Open("mysql", dsn)
+   	if err != nil {
+       		log.Fatalf("impossible to create the connection: %s", err)
+   	}
+   	defer db.Close()
+*/
 
 //}
