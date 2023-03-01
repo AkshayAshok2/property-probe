1de59e2dc266b7986f79461ebbe44fd667cf4b81
cseschmidt <113317598+cseschmidt@users.noreply.github.com>
2023-02-28 23:24:38 -0600

    Update properties_test.go

--- api/platform/properties/properties_test.go
+++ api/platform/properties/properties_test.go
@@ -8,24 +8,23 @@ import (
 	"gorm.io/gorm"
 )
 
-func Add(property Property) {
+func Connect() *gorm.DB {
 	dsn := "go:Gators123@tcp(cen3031-project.mysql.database.azure.com:3306)/listings?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
+	return db
+}
+
+func Add(db *gorm.DB, property Property) {
 	result := db.Create(property)
 	if result.Error != nil {
 		panic(result.Error)
 	}
 
 }
-func getSize() int {
-	dsn := "go:Gators123@tcp(cen3031-project.mysql.database.azure.com:3306)/listings?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
+func getSize(db *gorm.DB) int {
 	var properties []Property
 	resultCheckFirst := db.Find(&properties)
 	if resultCheckFirst.Error != nil {
@@ -34,22 +33,19 @@ func getSize() int {
 	return len(properties)
 }
 
-func TestAdd(t *testing.T) {
+func TestAdd(db *gorm.DB, t *testing.T) {
 	//Size before add function
-	var sizeInitial = getSize()
-	//Creates example property
+	var sizeInitial = getSize(db)
+	//Creates example property to add
 	property := Property{
 		auction_type:     "auction_type_1",
 		judgement_amount: 1000.0,
 		address:          "123 Main St",
 		assessed_value:   5000.0,
 	}
-
 	// Insert the property into the database
-	Add(property)
-
-	var sizeAfter = getSize()
-
+	Add(db, property)
+	var sizeAfter = getSize(db)
 	if sizeAfter <= sizeInitial {
 		t.Errorf("No property was added")
 	} else {
@@ -58,12 +54,8 @@ func TestAdd(t *testing.T) {
 
 }
 
-func printPropertiesTableDetails() {
-	dsn := "go:Gators123@tcp(cen3031-project.mysql.database.azure.com:3306)/listings?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
+func printPropertiesTableDetails(db *gorm.DB) {
+
 	var properties []Property
 	resultCheck := db.Find(&properties)
 	if resultCheck.Error != nil {
@@ -84,7 +76,7 @@ type Property struct {
 }
 
 func main() {
-	TestAdd(&testing.T{})
+	TestAdd(Connect(), &testing.T{})
 
 	/*property := Property{
 		auction_type:     "auction_type_1",
