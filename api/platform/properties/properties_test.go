559aa6833a7b2c824e9b6efe5d2d485e94f1e0d2
cseschmidt <113317598+cseschmidt@users.noreply.github.com>
2023-02-28 23:51:10 -0600

    Update properties_test.go

--- api/platform/properties/properties_test.go
+++ api/platform/properties/properties_test.go
@@ -33,9 +33,9 @@ func getSize(db *gorm.DB) int {
 	return len(properties)
 }
 
-func TestAdd(db *gorm.DB, t *testing.T) {
+func TestAdd(t *testing.T) {
 	//Size before add function
-	var sizeInitial = getSize(db)
+	var sizeInitial = getSize(Connect())
 	//Creates example property to add
 	property := Property{
 		auction_type:     "auction_type_1",
@@ -44,17 +44,17 @@ func TestAdd(db *gorm.DB, t *testing.T) {
 		assessed_value:   5000.0,
 	}
 	// Insert the property into the database
-	Add(db, property)
-	var sizeAfter = getSize(db)
+	Add(Connect(), property)
+	var sizeAfter = getSize(Connect())
 	if sizeAfter <= sizeInitial {
 		t.Errorf("No property was added")
 	} else {
-		fmt.Println("Test passed: property added")
+		fmt.Println("Property Successfully Added")
 	}
 
 }
 
-func printPropertiesTableDetails(db *gorm.DB) {
+/*func printPropertiesTableDetails(db *gorm.DB) {
 
 	var properties []Property
 	resultCheck := db.Find(&properties)
@@ -66,7 +66,7 @@ func printPropertiesTableDetails(db *gorm.DB) {
 		fmt.Println("ID: %d, AuctionType: %s, JudgementAmount: %f, PropertyAddress: %s, AssessedValue: %f\n",
 			property.auction_type, property.judgement_amount, property.address, property.assessed_value)
 	}
-}
+}*/
 
 type Property struct {
 	auction_type     string
@@ -76,14 +76,15 @@ type Property struct {
 }
 
 func main() {
-	TestAdd(Connect(), &testing.T{})
+	TestAdd(&testing.T{})
 
 	/*property := Property{
-		auction_type:     "auction_type_1",
-		judgement_amount: 1000.0,
-		address:          "123 Main St",
-		assessed_value:   5000.0,
-	}
-	Add(property)
-	printPropertiesTableDetails()*/
+			auction_type:     "auction_type_1",
+			judgement_amount: 1000.0,
+			address:          "123 Main St",
+			assessed_value:   5000.0,
+		}
+		Add(property)
+		printPropertiesTableDetails()
+	}*/
 }
