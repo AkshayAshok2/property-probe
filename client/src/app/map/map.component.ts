f9e20df7f46236fc632fca7ea96e0e79a75ddbf9
Technical-1 <jacobrk2001@gmail.com>
2023-03-21 22:41:29 -0400

    added different map center points based on inputted zipcode

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -13,11 +13,25 @@ import { fromLonLat } from 'ol/proj.js';
 export class MapMaker implements OnInit {
 
   map: Map = new Map;
-
+  
   ngOnInit(): void {
+    let centerCoordinates = [-82.324,29.654];
+    let zipCode = '32601'; 
+
+    // Check if zip code is in Gainesville and set center coordinates accordingly
+    if (zipCode === '32601') {
+      centerCoordinates = [-82.32146, 29.63964];
+    } else if (zipCode === '32603') {
+      centerCoordinates = [-82.35590, 29.65082];
+    } else if (zipCode === '32605') {
+      centerCoordinates = [-82.38564, 29.68129];
+    } else if (zipCode === '32606') {
+      centerCoordinates = [-82.44238, 29.68419];
+    }
+
     this.map = new Map({
       view: new View({
-        center: fromLonLat([-82.324,29.654]),
+        center: fromLonLat(centerCoordinates),
         zoom: 13,
       }),
       layers: [
