769868b97201d623e57a7017a87435a2cc8e5411
Technical-1 <jacobrk2001@gmail.com>
2023-03-28 17:04:28 -0400

    added property boxes on the left side of the page. added lon lat script to add to the text doc using google api

---
+++ longitudeLatitude.js
@@ -0,0 +1,84 @@
+
+// const getAddressLatLng = (address) => {
+//   const options = {
+//     headers: {
+//       'User-Agent': 'Property Probe'
+//     }
+//   };
+  
+//   return new Promise((resolve, reject) => {
+//     https.get(`https://nominatim.openstreetmap.org/search?q=${encodeURI(address)}&format=json`, options, (res) => {
+//       let data = '';
+//       res.on('data', (chunk) => {
+//         data += chunk;
+//       });
+//       res.on('end', () => {
+//         const results = JSON.parse(data);
+//         if (results.length > 0) {
+//           const lat = results[0].lat;
+//           const lon = results[0].lon;
+//           resolve({ lat, lon });
+//         } else {
+//           resolve(null);
+//         }
+//       });
+//     }).on('error', (err) => {
+//       reject(err);
+//     });
+//   });
+// };
+
+
+const fs = require('fs');
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const https = require('https');
+
+const API_KEY = 'AIzaSyDWV3QGYTAf4ScolWz4EWvrpLBm3KGVJH0'; 
+const getAddressLatLng = (address) => {
+  
+  return new Promise((resolve, reject) => {
+    https.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(address)}&key=${API_KEY}`, (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        const result = JSON.parse(data);
+        if (result.status === 'OK') {
+          const location = result.results[0].geometry.location;
+          resolve({ lat: location.lat, lon: location.lng });
+        } else {
+          resolve(null);
+        }
+      });
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+};
+
+const processFile = async (filename) => {
+  const data = await readFile(filename, 'utf8');
+  const lines = data.split('\n');
+  let output = '';
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    if (line.includes('Property Address')) {
+      const address = line.split(': ')[1];
+      const latLon = await getAddressLatLng(address);
+      if (latLon === null) {
+        output += `${line}\n[No results found]\n`;
+      } else {
+        const { lat, lon } = latLon;
+        output += `${line}\n[${lat}, ${lon}]\n`;
+      }
+    } else {
+      output += line + '\n';
+    }
+  }
+  await writeFile(filename, output, 'utf8');
+};
+
+processFile('properties.txt');
