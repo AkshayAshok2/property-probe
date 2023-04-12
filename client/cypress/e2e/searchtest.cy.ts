ad4529ed04f69f9fca6c60f9adcb7fc2ffe77cbe
Jacob Kanfer <51518860+Technical-1@users.noreply.github.com>
2023-04-12 14:57:58 -0400

    updated cypress

--- client/cypress/e2e/searchtest.cy.ts
+++ client/cypress/e2e/searchtest.cy.ts
@@ -1,13 +1,13 @@
 
 describe('SearchComponent', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:4200/') // replace with the path to your search component
+    cy.visit('http://localhost:4200/')
   })
 
   it('should allow the user to search for properties', () => {
-    cy.get('.app-search input[type="text"]').type('property') // type the search term into the input field
-    cy.get('.app-search button').click() // click the search button
+    cy.get('.app-search input[type="text"]').type('property') 
+    cy.get('.app-search button').click()
     cy.wait(2000)
-    cy.get('.app-search .searchHistory').contains('SearchTerm: property') // verify that the search history contains the search term
+    cy.get('.app-search .searchHistory').contains('SearchTerm: property') 
   })
 })
