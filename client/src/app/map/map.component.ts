f98e6851ae2d76853fc70579e3868cd76584ccf1
Jacob Kanfer <51518860+Technical-1@users.noreply.github.com>
2023-04-12 16:21:19 -0400

    made map markers work off backend lon lat vs dummy data

--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -9,17 +9,30 @@ import Point from 'ol/geom/Point';
 import { Icon, Style } from 'ol/style';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-
+import { HttpClient } from '@angular/common/http';
+import { interval, take, lastValueFrom } from 'rxjs';
+interface PropertyTerm {
+	latlon          :string
+}
 @Component({
   selector: 'map-container',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css'],
 })
 export class MapMaker implements OnInit {
+  public allProperties: PropertyTerm[] = []
+
+  constructor(
+    private httpClient: HttpClient
+  ){}
   
+  async loadProperties() {
+    this.allProperties = await lastValueFrom(this.httpClient.get<PropertyTerm[]>('/api/properties'))
+  }
   map: Map = new Map;
   
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.loadProperties()
     let centerCoordinates = [-82.324,29.654];
     let zipCode = '32601'; 
 
@@ -47,117 +60,32 @@ export class MapMaker implements OnInit {
     target: 'map',
     });
 
-    // Create a vector source for the markers
-    const markerSource = new VectorSource();
-
-    // create markers
-    const markers = [
-  {
-    name: 'Marker 1',
-    lon: -82.59850929999999,
-    lat: 29.81119469999999,
-  },
-  {
-    name: 'Marker 2',
-    lon: -82.4178211,
-    lat: 29.7390767,
-  },
-  {
-    name: 'Marker 3',
-    lon: -82.09298199999999,
-    lat: 29.542478,
-  },
-  {
-    name: 'Marker 4',
-    lon: -82.4886009,
-    lat: 29.793773,
-  },
-  {
-    name: 'Marker 5',
-    lon: -82.4037836,
-    lat: 29.68089729999999,
-  },
-  {
-    name: 'Marker 6',
-    lon: -82.5502978,
-    lat: 29.7281886,
-  },
-  {
-    name: 'Marker 7',
-    lon: -82.423918,
-    lat: 29.605522,
-  },
-  {
-    name: 'Marker 8',
-    lon: -82.5614209,
-    lat: 29.771643,
-  },
-  {
-    name: 'Marker 9',
-    lon: -82.30743460000001,
-    lat: 29.6772913,
-  },
-  {
-    name: 'Marker 10',
-    lon: -82.4188873,
-    lat: 29.63328079999999,
-  },
-  {
-    name: 'Marker 11',
-    lon: -82.29008739999999,
-    lat: 29.5031695,
-  },
-  {
-    name: 'Marker 12',
-    lon: -82.478787,
-    lat: 29.7571521,
-  },
-  {
-    name: 'Marker 13',
-    lon: -82.4360255,
-    lat: 29.6614827,
-  },
-  {
-    name: 'Marker 14',
-    lon: -82.524773,
-    lat: 29.608601,
-  },
-  {
-    name: 'Marker 15',
-    lon: -82.3347826,
-    lat: 29.69196789999999,
-  }
-    ].map((m) => {
+    // loop through properties and create features
+    const features = this.allProperties.map((property) => {
+      const [lon, lat] = property.latlon.replace('[','').replace(']','').split(',').map(Number);
       const feature = new Feature({
-        geometry: new Point(fromLonLat([m.lon, m.lat])),
-        name: m.name,
+        geometry: new Point(fromLonLat([lon, lat])),
       });
-
-      // create marker style
       const iconStyle = new Style({
         image: new Icon({
           anchor: [0.5, 1],
           src: 'https://cdn.mapmarker.io/api/v1/pin?size=50&background=%23006cfc&icon=fa-home&color=%23FFFFFF&voffset=0',
         }),
       });
-
       feature.setStyle(iconStyle);
-
-      // // add click event listener to marker
-      // this.map.on('click', function (evt) {
-      //   console.log('Marker clicked:', m.name);
-      // });
-
       return feature;
     });
 
-    // add markers to map
-    const vectorLayer = new VectorLayer({
-      source: new VectorSource({
-        features: markers,
-      }),
+    // create vector source and layer
+    const markerSource = new VectorSource({
+      features: features,
+    });
+    const markerLayer = new VectorLayer({
+      source: markerSource,
     });
 
-    this.map.addLayer(vectorLayer);
+    // add layer to map
+    this.map.addLayer(markerLayer);
+    
     } 
 }
