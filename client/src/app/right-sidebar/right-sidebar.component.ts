04740809761184fd32d1f60027ce6e09450606f1
Jacob Kanfer <51518860+Technical-1@users.noreply.github.com>
2023-04-12 15:47:29 -0400

    combined backend into frontend for grabbing properties

--- client/src/app/right-sidebar/right-sidebar.component.ts
+++ client/src/app/right-sidebar/right-sidebar.component.ts
@@ -3,9 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-right-sidebar',
   template: `
-    <div *ngIf="property">
-      <h2>{{ property.name }}</h2>
-      <p>{{ property.price }} - {{ property.sqFootage }}</p>
+
+    <div *ngIf="property" >
+      <h2 style="order: 1; width: 100%;flex-flow: row wrap;">{{ property.address }}</h2>
+      <p style="order: 2; column-count: 2;">{{ property.assessedvalue }} - {{ property.date }}</p>
+      <p> {{ property.description }}</p>
     </div>
   `,
 })
